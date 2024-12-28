b60b628c612b84aeca45cacbd72488f950c364c4
Zachran Razendra <zachranraze@recodex.id>
2024-12-29 03:48:20 +0700

    Set up a fresh Laravel app

---
+++ database/migrations/2024_12_28_191710_create_donations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->id();
+            $table->string('donor_name');
+            $table->decimal('amount', 10);
+            $table->string('payment_method');
+            $table->text('message')->nullable();
+            $table->string('status')->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donations');
+    }
+};
